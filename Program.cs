d329ed9367a73d754bd7a895e1b4d531ee5447b5
pedroldevjava <pedroldevjava@gmail.com>
2025-11-19 08:02:22 -0300

    CalculadoraAreaCirculo

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+
+namespace ProgramaCalculadora {
+
+    class Program {
+    static void Main(string[] args) {
+
+            Console.WriteLine("ENTRADA DE DADOS");
+
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double pi = 3.14159;
+
+            double area = pi * Math.Pow(raio, 2.0);
+            
+            Console.WriteLine("A= " + area.ToString("F4",CultureInfo.InvariantCulture)); // F4 = 4 casas decimais
+
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
